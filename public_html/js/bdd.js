cc2612f72b4f9fd66fa9052fa0fd510403e89a15
Bhadresh Desai <bhadresh_desai@hotmail.com>
2017-10-26 08:35:54 +0100

    Initial import

---
+++ public_html/js/bdd.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    //Toggle fullscreen
+    $(".cpanel-fullscreen").click(function (e) {
+        e.preventDefault();
+        
+        var $this = $(this);
+    
+        if ($this.children('i').hasClass('glyphicon-resize-full'))
+        {
+            $this.children('i').removeClass('glyphicon-resize-full');
+            $this.children('i').addClass('glyphicon-resize-small');
+        }
+        else if ($this.children('i').hasClass('glyphicon-resize-small'))
+        {
+            $this.children('i').removeClass('glyphicon-resize-small');
+            $this.children('i').addClass('glyphicon-resize-full');
+        }
+        $(this).closest('.panel').toggleClass('panel-fullscreen');
+    });
+});
