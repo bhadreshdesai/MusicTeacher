ef88e00df8129701e82bdd8ba0620c1d818ea4e3
Bhadresh Desai <bhadresh_desai@hotmail.com>
2017-10-27 07:34:33 +0100

    Load abc notation from a file

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 git clone https://github.com/bhadreshdesai/MusicTeacher.git
 
 Load public_html/index.html in your favourite browser
+
+Click Open File
+Choose cooley.abc from music folder or any abc notation file
+
+The file will be loaded in text box and music sheet will be displayed
+
+Alternatively enter the abc notation in the text box
+
--- public_html/js/bdd.js
+++ public_html/js/bdd.js
@@ -2,19 +2,32 @@ $(document).ready(function () {
     //Toggle fullscreen
     $(".cpanel-fullscreen").click(function (e) {
         e.preventDefault();
-        
+
         var $this = $(this);
-    
+
         if ($this.children('i').hasClass('glyphicon-resize-full'))
         {
             $this.children('i').removeClass('glyphicon-resize-full');
             $this.children('i').addClass('glyphicon-resize-small');
-        }
-        else if ($this.children('i').hasClass('glyphicon-resize-small'))
+        } else if ($this.children('i').hasClass('glyphicon-resize-small'))
         {
             $this.children('i').removeClass('glyphicon-resize-small');
             $this.children('i').addClass('glyphicon-resize-full');
         }
         $(this).closest('.panel').toggleClass('panel-fullscreen');
+    });
+
+    $("#fileToLoad").change(function (e) {
+        var fileToLoad = e.target.files[0];
+        if (fileToLoad) {
+            var reader = new FileReader();
+            reader.onload = function (fileLoadedEvent) {
+                var textFromFileLoaded = fileLoadedEvent.target.result;
+                $("#abc").val(textFromFileLoaded);
+                $("#abc").change();
+            };
+            reader.readAsText(fileToLoad, 'UTF-8');
+            $("#fileName").val(fileToLoad.name);
+        }
     });
 });
